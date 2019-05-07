ca930580be628236eea14baff94a1cf6acf49c61
yaojain <18239878516@139.com>
2019-05-07 10:20:18 +0800

    范

--- src/main.js
+++ src/main.js
@@ -1,22 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './routers'
 import store from './stores'
 import axios from 'axios'
-import BScroll from "better-scroll";
 Vue.config.productionTip = false; //设置为 false 以阻止 vue 在启动时生成生产提示
 Vue.prototype.$axios = axios;
 Vue.filter('setWh',(url,arg)=>{
-  console.log(url)
-  console.log(arg)
   return url.replace(/w\.h/,arg)
 })
 import Scroller from '@/components/Scroller'
 Vue.component("Scroller",Scroller);
 import Loading from '@/components/Loading'
 Vue.component("Loading",Loading);
+import router from './routers'
 new Vue({
-  router,
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,13 +1,14 @@
 import Vue from 'vue'
-import Router from 'vue-router'
 import moverRouter from './movie'
 import cinemaRouter from './cinema'
 import mineRouter from './mine'
+import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: process.env.BASE_URL,
+  // base: process.env.BASE_URL,
+  base : 'miaomiao',
   routes: [
     moverRouter,
     cinemaRouter,
